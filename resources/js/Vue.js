0713afcf1fef124169a10341168c32758e6db963
ManuelAvanzi <manuel.92.avanzi@gmail.com>
2022-03-15 00:11:20 +0100

    to fix something in app.js

--- resources/js/Vue.js
+++ resources/js/Vue.js
@@ -14,7 +14,7 @@ window.Vue = require('vue');
 
 //Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
-Import App from "./views/App.vue";
+//Import App from "./views/App.vue";
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
